f76a68716843e054b1b77b0a7acab2bb81a18534
kaskad <kaskad@bigmir.net>
2021-09-13 15:05:39 +0300

    adding starting files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 
-function App(){
-    return(
-        <h1>Hello!</h1>
-    )
+function App() {
+  return (
+    <div className="container">
+      <h1>TIME</h1>
+      <button>Get Time</button>
+    </div>
+  );
 }
 
 export default App;
